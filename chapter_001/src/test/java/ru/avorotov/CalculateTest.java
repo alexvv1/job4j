9c52d88f416bb14b73a610449eddad0f4e62ce8e
Alex Vorotov <vorotovalex@gmail.com>
2016-12-17 17:02:58 +0300

    Добавлены тесты для класса Calculate

---
+++ chapter_001/src/test/java/ru/avorotov/CalculateTest.java
@@ -0,0 +1,35 @@
+package ru.avorotov;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test Calculate Class
+ *
+ * @author avorotov
+ * @version 1.0
+ * @since 17.12.2016 
+ */
+public class CalculateTest {
+	
+	@Test
+	public void whenRunMainToPrintHelloWorld() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(
+			out.toString(),
+			is(
+				String.format(
+					"Hello World!%s",
+					System.getProperty("line.separator")
+				)
+			
+			)
+		);
+		
+	}
+}
