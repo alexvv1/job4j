e3c468c30e7f0852cd6477865669bb233605932b
sbt-vorotov1-av <AVVorotov1.SBT@sberbank.ru>
2018-02-07 10:38:56 +0300

    #39989 Контйнер на базе связанного списка.

---
+++ chapter_collection_pro/list/src/main/java/ru/vorotov/list/SimpleContainer.java
@@ -0,0 +1,30 @@
+package ru.vorotov.list;
+
+/**
+ * Интерфейс контейнера.
+ * @param <T> Тип хранимого элемента.
+ */
+public interface SimpleContainer<T> extends Iterable<T> {
+
+    /**
+     * Добавить элемент в контейнер.
+     *
+     * @param value Добавляемый элемент в контейнер.
+     */
+    void add(T value);
+
+    /**
+     * Возвращает элемен из контейнера по индексу.
+     *
+     * @param index Индекс элемента.
+     * @return элемен из контейнера по индексу.
+     */
+    T get(int index);
+
+    /**
+     * Размер контейнера.
+     *
+     * @return Размер контейнера.
+     */
+    int size();
+}
