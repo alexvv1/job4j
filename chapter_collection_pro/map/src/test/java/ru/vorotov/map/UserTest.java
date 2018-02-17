56b13da5622d57baca7f8c29d0ff203c610ac2c0
Alex Vorotov <vorotovalex@gmail.com>
2018-02-17 16:43:25 +0300

    #39996 Not Override Equals And HashCode

---
+++ chapter_collection_pro/map/src/test/java/ru/vorotov/map/UserTest.java
@@ -0,0 +1,38 @@
+package ru.vorotov.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User test.
+ */
+public class UserTest {
+    /**
+     * Equals and HashCode не переопределены. Оба user добавлены в Map.
+     * По умолчанию Hashcode для разные инстансов возвращает всегда разные значения.
+     * Значит объекты будут добавлены в коллекцию как разные.
+     */
+    @Test
+    public void whenNotOverrideEqualsAndHashCode() {
+        User user1 = new User("Name", 1, Calendar.getInstance());
+        User user2 = new User("Name", 1, Calendar.getInstance());
+        Map<User, Object> testMap = new HashMap<>();
+        testMap.put(user1, new Object());
+        testMap.put(user2, new Object());
+
+        printMap(testMap);
+    }
+
+    /**
+     * Print map.
+     * @param map Map for printing.
+     */
+    private void printMap(Map<?, ?> map) {
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println("Key:" + entry.getKey() + "; Value:" + entry.getValue());
+        }
+    }
+}
