72016afcfd84cda37f6de455d9f4935f58205330
Alex Vorotov <vorotovalex@gmail.com>
2018-02-17 16:53:05 +0300

    #39997 Override HashCode

---
+++ chapter_collection_pro/map/src/main/java/ru/vorotov/map/UserHashCode.java
@@ -0,0 +1,30 @@
+package ru.vorotov.map;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+/**
+ *
+ */
+public class UserHashCode extends User {
+
+    /**
+     * Ctor.
+     *
+     * @param name     Name.
+     * @param children Children.
+     * @param birthday Birthday.
+     */
+    public UserHashCode(String name, int children, Calendar birthday) {
+        super(name, children, birthday);
+    }
+
+    /**
+     * HashCode.
+     * @return HashCode.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getChildren(), getBirthday());
+    }
+}
--- chapter_collection_pro/map/src/test/java/ru/vorotov/map/UserTest.java
+++ chapter_collection_pro/map/src/test/java/ru/vorotov/map/UserTest.java
@@ -27,6 +27,26 @@ public class UserTest {
     }
 
     /**
+     * HashCode переопределен. Оба user добавлены в Map.
+     * т.к. добавляемые объекты равны то и HashCode равны.
+     * Далее идет сравнение методом Equals.
+     * По-умолчнию метод Equals возвращает false для разных инстанцов оъектов
+     *
+     * Следовательно объекты будут добавлены в коллекцию как разные.
+     */
+    @Test
+    public void whenOverrideHashCode() {
+        User user1 = new UserHashCode("Name", 1, Calendar.getInstance());
+        User user2 = new UserHashCode("Name", 1, Calendar.getInstance());
+        Map<User, Object> testMap = new HashMap<>();
+        testMap.put(user1, new Object());
+        testMap.put(user2, new Object());
+
+        printMap(testMap);
+    }
+
+
+    /**
      * Print map.
      * @param map Map for printing.
      */
